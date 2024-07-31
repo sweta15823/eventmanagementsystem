dfe980c5e798a4a97d163b99c0a759f582540abb
sweta15823 <162858420+sweta15823@users.noreply.github.com>
2024-07-31 21:16:13 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,38 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>P</strong><span class = "small footerSubtext">rojectworlds of</span>
+                        <strong>I</strong><span class = "small footerSubtext">nstitute</span>
+                        <strong>D</strong><span class = "small footerSubtext">esign and</span>
+                        <strong>T</strong><span class = "small footerSubtext">echnology</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Kill Avenue, Naya Raipur,
+                        &copy; Projectworlds 2018.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    DL824 Multimedia Design/Web Engineering<br>
+                    Projectworlds.in 2017-2018.<br>
+                    Naya Raipur, 492001
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
